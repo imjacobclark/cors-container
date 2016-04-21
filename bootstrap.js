02240ca8406bf7e468ceec9530192a77138bcaab
mrmx <mistermx@gmail.com>
2016-04-21 18:29:54 +0200

    Some format changes and fixes

--- bootstrap.js
+++ bootstrap.js
@@ -3,14 +3,15 @@
 const request = require('request-promise');
 const converter = require('rel-to-abs');
 const url = require('url');
-const util = require('util');
 const fs = require('fs');
 const index = fs.readFileSync('index.html', 'utf8');
 const debug = process.env.debug_log || false;
 
 module.exports = function(app){
     function setHeaders(res, origin){
-        if(debug) console.info("setHeaders origin:" , JSON.stringify(origin.headers));
+        if(debug) {
+            console.info("setHeaders origin:" , JSON.stringify(origin.headers));  
+        } 
         res.header(origin.headers);
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Credentials', false);
@@ -19,25 +20,27 @@ module.exports = function(app){
     }
 
     app.get('/*', (req, res) => {
-        let origionalUrl = req.originalUrl;
+        let originalUrl = req.originalUrl;
         let requestedUrl = req.params[0];
-        let reqUrl = url.parse(requestedUrl)
+        let parsedRequestUrl = url.parse(requestedUrl)
         let corsBaseUrl = '//' + req.get('host');
         
-        console.info(req.protocol + '://' + req.get('host') + origionalUrl);
+        console.info(req.protocol + '://' + req.get('host') + originalUrl);
         
         if(requestedUrl == ''){
             res.send(index);
             return;
         }        
-        if(!reqUrl.host) {
+        if(!parsedRequestUrl.host) {
             res.status(400);
-            res.send(util.format("Invalid Url '%s'",requestedUrl));
+            res.send("Invalid Url '" + requestedUrl + "'"));
             return
         }
         
-        req.headers['host'] = reqUrl.host;
-        if(debug) console.info("Req headers:" , JSON.stringify(req.headers));
+        req.headers['host'] = parsedRequestUrl.host;
+        if(debug) {
+            console.info("Req headers:" , JSON.stringify(req.headers));
+        }
 
         request({
             uri: requestedUrl,
